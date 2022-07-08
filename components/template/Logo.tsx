5bb5fa7f04e8b4b6fdc4d557f05959ed8b529667
CalebeSG <calebeguimaraes10@gmail.com>
2022-07-08 12:48:08 -0300

    style: adjustments

--- components/template/Logo.tsx
+++ components/template/Logo.tsx
@@ -1,10 +1,10 @@
 export function Logo() {
   return (
-    <div className="flex gap-1 flex-col items-center justify-center w-10 h-10 rounded-full bg-white">
-      <div className="bg-red-600 w-2 h-2 rounded-full" />
-      <div className="flex gap-1">
-        <div className="bg-yellow-500 w-2 h-2 rounded-full" />
-        <div className="bg-green-500 w-2 h-2 rounded-full" />
+    <div className="flex flex-col items-center justify-center w-10 h-10 rounded-full bg-white">
+      <div className="bg-red-600 w-2.5 h-2.5 rounded-full" />
+      <div className="flex gap-0.5">
+        <div className="bg-yellow-500 w-2.5 h-2.5 rounded-full" />
+        <div className="bg-green-500 w-2.5 h-2.5 rounded-full" />
       </div>
     </div>
   )
--- components/template/ThemeButton.tsx
+++ components/template/ThemeButton.tsx
@@ -13,11 +13,11 @@ export function ThemeButton(props: ThemeButtonProps) {
       className="hidden sm:flex flex-row-reverse items-center cursor-pointer rounded-full w-14 lg:w-24 h-8 p-1 bg-gradient-to-r from-cyan-500 to-purple-900 dark:from-yellow-300 dark:to-yellow-600 dark:flex-row transition-all"
       onClick={props.onChange}
     >
-      <div className="bg-white rounded-full dark:text-yellow-600 text-blue-600 w-6 h-6 flex items-center justify-center transition-colors">
+      <div className="w-6 h-6 flex items-center justify-center rounded-full bg-white text-blue-600 dark:text-yellow-600 transition-colors">
         {Icon('w-4 h-4')}
       </div>
 
-      <div className="hidden flex-1 lg:flex items-center justify-end pr-2">
+      <div className="flex-1 hidden lg:flex items-center justify-center">
         <span className="text-sm text-white">
           {props.value === 'dark' ? 'Claro' : 'Escuro'}
         </span>
