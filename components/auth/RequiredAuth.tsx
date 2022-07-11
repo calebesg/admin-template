b7a25ee9137644fff9acd8fc08f77d46676ef03b
CalebeSG <calebeguimaraes10@gmail.com>
2022-07-11 09:15:03 -0300

    new protection in routes

--- components/auth/RequiredAuth.tsx
+++ components/auth/RequiredAuth.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Image from 'next/image'
 import Router from 'next/router'
 import { useAuth } from '../../data/hook'
@@ -9,7 +10,22 @@ interface RequiredAuthProps {
 export function RequiredAuth({ children }: RequiredAuthProps) {
   const { user, loading } = useAuth()
 
-  const renderContent = () => children
+  const renderContent = () => (
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if(!document.cookie?.includes("admin-template-firebase")) {
+                window.location.href = "/authentication"
+              }
+            `,
+          }}
+        />
+      </Head>
+      {children}
+    </>
+  )
 
   const renderSpinner = () => (
     <div className="flex justify-center items-center h-screen">
